d0a5556730252cf95ecc67fe2e6730aca6034e25
Albert Krewinkel <albert@zeitkraut.de>
2017-01-02 22:12:02 +0100

    Add nullary inline data constructors

--- src/panluna.lua
+++ src/panluna.lua
@@ -1,25 +1,71 @@
 --[[
-  panluna.lua
+panluna.lua
 
-  Copyright (c) 2017 Albert Krewinkel
+Copyright (c) 2017 Albert Krewinkel
 
-  Permission to use, copy, modify, and/or distribute this software for any purpose
-  with or without fee is hereby granted, provided that the above copyright notice
-  and this permission notice appear in all copies.
+Permission to use, copy, modify, and/or distribute this software for any purpose
+with or without fee is hereby granted, provided that the above copyright notice
+and this permission notice appear in all copies.
 
-  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-  REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
-  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-  INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
-  OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
-  TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
-  THIS SOFTWARE.
-
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+THIS SOFTWARE.
 ]]
 local _version = "0.0.1"
 
-local M = {
-  _version = _version
+-- Types
+local MetaType = {}
+-- Create a subtype
+MetaType.__call =  function (ty, tag, ...)
+  local t = {tag = tag}
+  setmetatable(t, ty)
+  ty.__index = ty
+  return t
+end
+MetaType.__index = MetaType
+
+local Type = {}
+setmetatable(Type, MetaType)
+Type.__index = Type
+Type.__call = MetaType.__call
+-- Create an instance of the type
+function Type:new(o)
+  o = o or {}
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+
+-- Inline elements
+local Inline = Type "Inline"
+Inline.definitions = {
+  LineBreak = Inline "LineBreak",
+  SoftBreak = Inline "SoftBreak",
+  Space     = Inline "Space",
 }
 
-return M
+-- Convert to JSON structure
+function Inline:to_json_structure()
+  if next(self) ~= nil then
+    return {t = self.tag, c = self}
+  else
+    return {t = self.tag}
+  end
+end
+
+-- Initialize from JSON structure
+function Inline.from_json_structure(x)
+  return Inline.definitions[x.t]:new(x.c)
+end
+
+
+-- Return everything that should be exported from the module
+return {
+  _version = _version,
+  Inline = Inline
+}
